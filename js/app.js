619f15a83ef4f5781921b102f26d9762b82e8412
Wilfried <la2spaille@gmail.com>
2022-01-03 13:05:40 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const allPlus = document.querySelectorAll('.plus')
 const allMinus = document.querySelectorAll('.minus')
-const allPrice = document.querySelectorAll('.price')
-const allItems = document.querySelectorAll('.items')
+var allPrice = document.querySelectorAll('.price')
+var allItems = document.querySelectorAll('.items')
 const total = document.querySelector('.total')
+var decimal
+var decimalFixed
+
 allPlus.forEach((plus,index) => {
     plus.addEventListener('click' , (e) => {
         allItems[index].textContent = allItems[index].textContent - -1;
@@ -14,7 +17,9 @@ allMinus.forEach((minus,index) => {
     minus.addEventListener('click' , (e) => {
         if (allItems[index].textContent > 1) {
             allItems[index].textContent = allItems[index].textContent  - 1;
-            total.textContent = total.textContent - allPrice[index].textContent
+            decimal = total.textContent - allPrice[index].textContent
+            decimalFixed = decimal.toFixed(2)
+            total.textContent = decimalFixed
             e.stopPropagation()
         }
     })
